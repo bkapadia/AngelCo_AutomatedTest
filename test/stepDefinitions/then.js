466599c3e49b8cedc41a914d2884915e8a2ecd44
bkapadia <Bhavin@Bhavins-MacBook-Pro-2.local>
2019-03-04 01:23:40 -0500

    update

--- test/stepDefinitions/then.js
+++ test/stepDefinitions/then.js
@@ -1,10 +1,26 @@
 import {defineSupportCode } from 'cucumber';
 import angelCo from '../pageobjects/home.page';
 
+module.exports = new Promise(function(resolve, reject) {
+  // async code here;
+
 defineSupportCode(function({ Then }) {
 
   Then(/^I should be on registration page$/, function() {
     angelCo.signupPage();
   });
 
+  Then(/^I submit form$/, function() {
+    angelCo.submit();
+  });
+
+  Then(/^I submit verification if possible$/, function() {
+    angelCo.submit();
+  });
+
+  Then(/^I should be logged in$/, function() {
+    angelCo.loginVerify();
+  });
+  
+});
 });
--- test/stepDefinitions/when.js
+++ test/stepDefinitions/when.js
@@ -2,7 +2,8 @@
 import { defineSupportCode } from 'cucumber';
 import angeloCo from '../pageobjects/home.page';
 
-
+module.exports = new Promise(function(resolve, reject) {
+  // async code here;
 defineSupportCode(function({ When }) {
 
   When(/I am on Angellist homepage/, function() {
@@ -21,10 +22,35 @@ defineSupportCode(function({ When }) {
     angeloCo.register(arg1);
   });
 
-  // *** belongs to ta-loging  feature
   When(/^I login with username and password "([^"]*)" into the text box$/, function(arg1) {
     angeloCo.register(arg1);   
   });
 
+  When(/^I am logged in and I click on logout$/, function(arg1) {
+    angeloCo.logout();   
+  });
+
+  When(/^I login with a "([^"]*)" account$/, function(arg1) {
+    angeloCo.signinValid(arg1);   
+  });
+
+  When('I go to Angellist login page', function(callback) {
+    this.browser
+    .click("auth.login")
+    .getTitle().should.equal('Log In - AngelList').catch (function(err){
+      console.log(err)
+    })
+  })
+  
+  // When(/^I go to Angellist login page$/, function(arg1) {
+  //   angeloCo.login();   
+  // });
+
+  When(/^I enter existing valid credentials$/, function(arg1) {
+    angeloCo.signinExisting();   
+  });
+  
+  
+});
 
 });
